f623c9c1b8d82bc1eadf2bb73f0d7ec413c8506b
Zeyad <zeyadahmed@me.com>
2026-01-29 00:15:02 -0400

    report values of alpha_i < 0.1 instead of alpha_i < 1.0

--- r8/params-verification/get-percent-of-words.py
+++ r8/params-verification/get-percent-of-words.py
@@ -4,22 +4,25 @@ import numpy as np
 np.random.seed(42)
 df = pd.read_csv("../reports/r8-bb-params-mle.csv")
 
-# find prop of words with alpha_i < 1
-p_alpha_i_lt_1 = len(df[df['alpha_i'] < 1]) / len(df)
-print("Prop of words with alpha_i < 1:", p_alpha_i_lt_1)
+# find prop of words with alpha_i < 0.1
+p_alpha_i_lt_0_1 = len(df[df['alpha_i'] < 0.1]) / len(df)
+print("Prop of words with alpha_i < 0.1:", p_alpha_i_lt_0_1)
 
 # find prop of words with alpha_not_i > 10
 p_alpha_not_i_gt_10 = len(df[df['alpha_not_i'] > 10]) / len(df)
 print("Prop of words with alpha_not_i > 10:", p_alpha_not_i_gt_10)
 
-# print representative words with alpha_i < 1 and alpha_not_i > 10
-print("Representative words with alpha_i < 1 and alpha_not_i > 10:")
-print(df[(df['alpha_i'] < 1) & (df['alpha_not_i'] > 10)][['term', 'alpha_i', 'alpha_not_i']].sample(20))
+# print representative words with alpha_i < 0.1 and alpha_not_i > 10
+print("Representative words with alpha_i < 0.1 and alpha_not_i > 10:")
+print(df[(df['alpha_i'] < 0.1) & (df['alpha_not_i'] > 10)][['term', 'alpha_i', 'alpha_not_i']].sample(20))
 
-# find prop of words with alpha_i > 1 or alpha_not_i < 10
-p_alpha_i_gt_1_or_alpha_not_i_lt_10 = len(df[(df['alpha_i'] > 1) | (df['alpha_not_i'] < 10)]) / len(df)
-print("Prop of words with alpha_i > 1 or alpha_not_i < 10:", p_alpha_i_gt_1_or_alpha_not_i_lt_10)
+# find prop of words with alpha_i > 0.1 or alpha_not_i < 10
+p_alpha_i_gt_0_1_or_alpha_not_i_lt_10 = len(df[(df['alpha_i'] > 0.1) | (df['alpha_not_i'] < 10)]) / len(df)
+print("Prop of words with alpha_i > 0.1 or alpha_not_i < 10:", p_alpha_i_gt_0_1_or_alpha_not_i_lt_10)
 
 # print other representative words with the other way around (OR) and print their alphas  
-print("Representative words with alpha_i > 1 or alpha_not_i < 10:")
-print(df[(df['alpha_i'] > 1) | (df['alpha_not_i'] < 10)][['term', 'alpha_i', 'alpha_not_i']].sample(20))
+print("Representative words with alpha_i > 0.1 or alpha_not_i < 10:")
+print(df[(df['alpha_i'] > 0.1) | (df['alpha_not_i'] < 10)][['term', 'alpha_i', 'alpha_not_i']].sample(20))
+
+print("top 10 words with alpha_i > 1")
+print(df[df['alpha_i'] > 1][['term', 'alpha_i', 'alpha_not_i']].sample(10))
